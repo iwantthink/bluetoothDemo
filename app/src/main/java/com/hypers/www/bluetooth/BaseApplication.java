a3c7a47373398c329a814a1f8d21911a79c4a212
renbo <mychocolateisus@gmail.com>
2017-06-13 11:54:56 +0800

    update

--- app/src/main/java/com/hypers/www/bluetooth/BaseApplication.java
+++ app/src/main/java/com/hypers/www/bluetooth/BaseApplication.java
@@ -13,7 +13,6 @@ import com.umeng.socialize.UMShareAPI;
 public class BaseApplication extends Application {
     {
         PlatformConfig.setWeixin("wxf83c996d3fa9f5f1", "59c9ac2f053a3b73585ca6b417d9a12a");
-        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
     }
 
