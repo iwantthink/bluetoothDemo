f9fcd3b62a9d3684349e32ad318dc8649935988f
renbo <mychocolateisus@gmail.com>
2017-06-20 11:47:00 +0800

    release 1.0

--- app/src/main/java/com/hypers/www/bluetooth/base/BaseApplication.java
+++ app/src/main/java/com/hypers/www/bluetooth/base/BaseApplication.java
@@ -13,7 +13,7 @@ import com.umeng.socialize.UMShareAPI;
 public class BaseApplication extends Application {
     {
         PlatformConfig.setWeixin("wxf83c996d3fa9f5f1", "59c9ac2f053a3b73585ca6b417d9a12a");
-        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
+        PlatformConfig.setSinaWeibo("2290967974", "4a6a23f35f7eb19fcbbd212b0a77c4dd", "http://sns.whalecloud.com");
     }
 
 
--- app/src/test/java/com/hypers/www/bluetooth/ExampleUnitTest.java
+++ app/src/test/java/com/hypers/www/bluetooth/ExampleUnitTest.java
@@ -1,18 +1,5 @@
 package com.hypers.www.bluetooth;
 
-import com.hypers.www.bluetooth.home.p.HomePresent;
-import com.hypers.www.bluetooth.home.v.HomeActivity;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static junit.framework.Assert.assertEquals;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -20,36 +7,30 @@ import static junit.framework.Assert.assertEquals;
  */
 public class ExampleUnitTest {
 
-    @Mock
-    HomeActivity mHomeActivity;
-
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void atomicIntegerTest() {
-        AtomicInteger integer = new AtomicInteger();
-        integer.compareAndSet(0, 2);
-        assertEquals(2, integer.get());
-//        Log.d("HomeActivity", "integer.get():" + integer.get());
-
-        AtomicReference atomicReference;
-    }
-
-    private HomePresent mHomePresent;
-
-    @Before
-    public void setUp() {
-        mHomePresent = new HomePresent(mHomeActivity);
-    }
-
-    @Test
-    public void checkHomePresent() {
-        mHomePresent.openBle();
-        Mockito.verify(mHomePresent).openBle();
-    }
+//    @Mock
+//    HomeActivity mHomeActivity;
+//
+//    @Test
+//    public void addition_isCorrect() throws Exception {
+//        assertEquals(4, 2 + 2);
+//    }
+//
+//    @Test
+//    public void atomicIntegerTest() {
+//        AtomicInteger integer = new AtomicInteger();
+//        integer.compareAndSet(0, 2);
+//        assertEquals(2, integer.get());
+////        Log.d("HomeActivity", "integer.get():" + integer.get());
+//
+//        AtomicReference atomicReference;
+//    }
+//
+//    private HomePresent mHomePresent;
+//
+//    @Before
+//    public void setUp() {
+//        mHomePresent = new HomePresent(mHomeActivity);
+//    }
 
 
 }
